82108bdcb38ae677ad0817704404fab9e0c869c1
Richard Rosario <richardrosariojr@gmail.com>
2019-04-05 12:14:50 -0400

    first commit

---
+++ Pandas_2_mySQL-Script.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import mysql.connector
+from sqlalchemy import create_engine
+
+df = pd.read_csv('FMEL_Dataset.csv')
+engine = create_engine('mysql+mysqlconnector://root:Chisca123!@localhost:3306/Giraffe', echo=False)
+
+df.to_sql(name='test2', con=engine, if_exists = 'append', index=False)
+
+#This script translates CSV files into DF and then into a mysql databse
+
+#Tailor line 6 to the database platform of your choice and the specific database
+
+#have not tested for excel, just csv
+
+
+
